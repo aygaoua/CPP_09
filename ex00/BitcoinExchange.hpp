4d1e9293c1a544b32604fdc8816463c8b249c3bf
aygaoua <zgaoua72@gmail.com>
2025-03-27 03:37:56 +0000

    ex00 to push

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -8,10 +8,10 @@
 #include <map>
 #include <cmath>
 
-void btc(char **av);
-bool is_number(std::string num);
-bool check_day(std::string date);
-bool sup_than_k(std::string num);
-float process_data(std::map<std::string, float> data, std::string date);
+bool    is_number(std::string num);
+bool    check_day(std::string date);
+bool    sup_than_k(std::string num);
+float   process_data(std::map<std::string, float> data, std::string date);
+void    btc(char **av);
 
 #endif
